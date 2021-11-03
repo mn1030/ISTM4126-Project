bfad5627688a53cae6653672bdc043de6b59dfbc
mnguyen1030 <62580830+mnguyen1030@users.noreply.github.com>
2021-11-03 15:57:04 -0400

    Uploaded the beginning of the Restaurant class
    Will include a method that allows you to set menu items, prone to change

---
+++ Restaruant.java
@@ -0,0 +1,24 @@
+public class Restaurant{
+  /*
+private String name: Name of restaurant
+private String [] menu: Menu items and prices listed
+private String hours: Restaurant business hours
+private String cuisineType: Categories of cuisine type
+private Point (for calculating distance??) or Google Map coordinates
+int x
+int y
+private boolean gWorld: Does this restaurant take GWorld payment?
+
+  */
+
+
+  Restaurant{
+    //constructor
+  }
+
+  public void setMenu{
+    //set menu items
+  }
+
+
+}

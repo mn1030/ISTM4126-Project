3c263fdd3665647d62ca196d2d14daea14166008
mnguyen1030 <62580830+mnguyen1030@users.noreply.github.com>
2021-12-07 20:11:38 -0500

    updated colors and layout

--- RestaurantFinder/app/src/main/java/com/example/restaurantfinder/MainActivity.java
+++ RestaurantFinder/app/src/main/java/com/example/restaurantfinder/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.content.ContentValues;
@@ -20,11 +21,18 @@ public class MainActivity extends AppCompatActivity {
     private Button searchButton;
     private DBHandler dbHandler;
 
+    //references for programmatically manipulated features
+    private Switch gWorld;
+    private Spinner cuisType;
+    private int maxDist = 5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get references from manipulated filters
     }
 
 }

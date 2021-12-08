aa4af98b458d8cb5cc4cafa8f8b9906470af7fd2
mnguyen1030 <62580830+mnguyen1030@users.noreply.github.com>
2021-12-08 00:17:26 -0500

    Updated connectivity

--- RestaurantFinder/app/src/main/java/com/example/restaurantfinder/MainActivity.java
+++ RestaurantFinder/app/src/main/java/com/example/restaurantfinder/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -22,17 +24,64 @@ public class MainActivity extends AppCompatActivity {
     private DBHandler dbHandler;
 
     //references for programmatically manipulated features
-    private Switch gWorld;
+    private boolean use_gWorld;
     private Spinner cuisType;
     private int maxDist = 5;
+
+    private TextView distTextView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //get references from manipulated filters
+
+        Switch gWorld = (Switch) findViewById(R.id.gworld_switch); //GWORLD SWITCH
+
+        if (gWorld.isChecked()){
+            use_gWorld = false;
+        }
+        else{
+            use_gWorld = true;
+        }
+
+        cuisType = (Spinner) findViewById(R.id.types); //SPINNER
+        String cuisine = String.valueOf(cuisType.getSelectedItem()); //SAVE OUR CUISINETYPE!!
+
+        SeekBar distSeekBar = (SeekBar) findViewById(R.id.distSeekBar); //SEEKBAR
+        distSeekBar.setOnSeekBarChangeListener(seekBarListener);
+
+        distTextView = (TextView) findViewById(R.id.distTextView); //SEEKBAR TEXTVIEW
+        //distTextView.setText(maxDist+ " miles from Duques");
+    }
+
+    //listener object for the SeekBar's progress changed events
+    private SeekBar.OnSeekBarChangeListener seekBarListener =
+            new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    maxDist = progress; //SAVE OUR MAXDIST!!
+                    distTextView.setText(maxDist+ " miles from Duques");
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            };
+
+    public void buttonOnClick(View view){
+        //Read through data and filter based on maxDist, use_gWorld, cuisine
     }
 
 }
